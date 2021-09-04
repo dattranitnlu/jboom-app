15cc296c73d84e1f5d7802dd9669f1661ef97a27
Dat Tran <trandatitnlu@gmail.com>
2021-09-04 16:30:00 +0700

    fix(frontend): adding README.md, fixed impact for ghost

---
+++ README.md
@@ -0,0 +1,66 @@
+<h3 align="center">
+    JAVA SWING: MINI BOOM ZING GAME
+</h3>
+
+---
+
+Description
+------------
+
+Apply with design patterns:
+- Strategy pattern
+- MVC architecture
+- Observer pattern
+
+Link Demo
+------------
+```text
+https://www.youtube.com/watch?v=mx8D7rjwwfc
+```
+
+### Some features
+
+<p align="center">
+    <img src="/images/UserGuide.PNG" alt="User Guide interface">
+</p>
+
+<p align="center">
+    <img src="/images/SavingHighScore.PNG" alt="Saving High Score interface">
+</p>
+
+<p align="center">
+    <img src="/images/ChoosingHero.PNG" alt="Choosing Hero interface">
+</p>
+
+<p align="center">
+    <img src="/images/FirstGame.PNG" alt="Beginning Game 1 interface">
+</p>
+
+Installation
+------------
+
+Pre-conditions:
+- JDK: [Oracle JDK version 8](https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html)
+- IDE: IntelliJ IDEA Ultimate(recommended) or eclipse,...
+
+### Clone repo
+
+``` bash
+git clone https://github.com/hetsotchet12345/jboom-app.git
+```
+
+### Open project
+
+``` bash
+cd jboom-app/
+```
+
+### Run project
+
+Run class "src/main/RunGame.java"
+
+<p align="center">
+    <img src="/images/WelcomeGame.PNG" alt="Welcome game interface">
+</p>
+
+###### _Done!_
---
+++ src/controller/MenuControllerExt.java
@@ -0,0 +1,98 @@
+package controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+
+import model.ReadFileHightScore;
+import sound.GameSound;
+import view.GameView;
+import view.MenuView;
+
+
+/**
+ * @author Dat Tran
+ *
+ */
+public class MenuControllerExt extends ControllerView {
+	private MenuView menu;
+	private ControllerView controllerView;
+
+	public MenuControllerExt(GameView view, ControllerView controllerView) {
+		super(view);
+		this.controllerView = controllerView;
+		menu = this.view.myContainer.getMenu();
+		menu.addMenuMouseListener(new MenuMouseListener());
+	}
+
+	class MenuMouseListener extends MouseAdapter {
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			if (e.getSource() == menu.getLbPlayGame()) {
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				ImageIcon playIcon = new ImageIcon(getClass().getResource("/Images/Play2.png"));
+				menu.getLbPlayGame().setIcon(playIcon);
+			}
+			if (e.getSource() == menu.getLbOption()) {
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				ImageIcon optionIcon = new ImageIcon(getClass().getResource("/Images/Option2.png"));
+				menu.getLbOption().setIcon(optionIcon);
+			}
+			if (e.getSource() == menu.getLbHigthScore()) {
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				ImageIcon hightScoreIcon = new ImageIcon(getClass().getResource("/Images/HightScore2.png"));
+				menu.getLbHigthScore().setIcon(hightScoreIcon);
+			}
+			if (e.getSource() == menu.getLbExit()) {
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				ImageIcon exitIcon = new ImageIcon(getClass().getResource("/Images/Exit2.png"));
+				menu.getLbExit().setIcon(exitIcon);
+			}
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			if (e.getSource() == menu.getLbPlayGame()) {
+				ImageIcon playIcon = new ImageIcon(getClass().getResource("/Images/Play.png"));
+				menu.getLbPlayGame().setIcon(playIcon);
+			}
+			if (e.getSource() == menu.getLbOption()) {
+				ImageIcon optionIcon = new ImageIcon(getClass().getResource("/Images/Option.png"));
+				menu.getLbOption().setIcon(optionIcon);
+			}
+			if (e.getSource() == menu.getLbHigthScore()) {
+				ImageIcon hightScoreIcon = new ImageIcon(getClass().getResource("/Images/HightScore.png"));
+				menu.getLbHigthScore().setIcon(hightScoreIcon);
+			}
+			if (e.getSource() == menu.getLbExit()) {
+				ImageIcon exitIcon = new ImageIcon(getClass().getResource("/Images/Exit.png"));
+				menu.getLbExit().setIcon(exitIcon);
+			}
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			if (e.getSource() == menu.getLbExit()) {
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				System.exit(0);
+			}
+			if (e.getSource() == menu.getLbPlayGame()) {
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				view.myContainer.setShowActor();
+
+			}
+			if (e.getSource() == menu.getLbOption()) {
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				view.myContainer.setShowOption();
+			}
+			if (e.getSource() == menu.getLbHigthScore()) {
+				controllerView.setModel(new ReadFileHightScore());
+
+				GameSound.getIstance().getAudio(GameSound.BOMB).play();
+				view.myContainer.setShowHightScore();
+			}
+		}
+	};
+}
